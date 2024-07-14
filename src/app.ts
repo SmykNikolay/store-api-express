c8cf944eda806923f01a66bdd2d4ccbd82debb0b
Николай Смык <nikolaysmyk@Nikolajs-MacBook-Air.local>
2024-07-14 03:01:52 +0300

    Squash merge some into main

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import mongoose from 'mongoose';
-import express, { Request, Response, NextFunction } from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
 import { errors } from 'celebrate';
+import express, { NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
 
-import { IError } from './utils/types';
 import { DEFAULT_PORT } from './utils/constants';
+import { IError } from './utils/types';
 
-import userRoutes from './routes/userRoutes';
 import cardRoutes from './routes/cardRoutes';
 import orderRoutes from './routes/orderRoutes';
+import userRoutes from './routes/userRoutes';
 
 import { ERROR_MESSAGES, NotFoundError, STATUS_CODES } from './utils/errors';
 
-import { requestLogger, errorLogger } from './middlewares/logger';
+import { errorLogger, requestLogger } from './middlewares/logger';
 import categoryRoutes from './routes/categoryRoutes';
 import productRoutes from './routes/productRoutes';
 
@@ -41,13 +41,18 @@ app.use(
   })
 );
 
-app.use((err: any, _req: Request, _res: Response, next: NextFunction) => {
+app.use((err: Error, _req: Request, _res: Response, next: NextFunction) => {
   errorLogger.error(err);
   next(err);
 });
 
 app.get('/test', (_req, res) => {
   res.send('Hello world!');
+});
+
+app.get('/crash-test', (_req, res) => {
+  res.status(STATUS_CODES.INTERNAL_SERVER_ERROR);
+  throw new Error('Сервер сейчас упадёт');
 });
 
 app.use(userRoutes);
